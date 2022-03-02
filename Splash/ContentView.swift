62c16a1391f84722afd19f4f9a0a02b2ab0895d9
Shreeya Gad <sg988@cornell.edu>
2022-03-01 16:06:58 -0800

    Initial Commit

---
+++ Splash/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Splash
+//
+//  Created by Shreeya Gad on 3/1/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Splash/SplashApp.swift
@@ -0,0 +1,17 @@
+//
+//  SplashApp.swift
+//  Splash
+//
+//  Created by Shreeya Gad on 3/1/22.
+//
+
+import SwiftUI
+
+@main
+struct SplashApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
