e64321abbef9c65ba55c77012d5ef8efd191015c
Shreeya Gad <sg988@cornell.edu>
2022-03-04 02:56:56 -0800

    Organizing view into folders

---
+++ Splash/Models/DataController.swift
@@ -0,0 +1,26 @@
+//
+//  DataController.swift
+//  Splash
+//
+//  Created by Shreeya Gad on 3/2/22.
+//
+
+import CoreData
+import SwiftUI
+
+class DataController: ObservableObject {
+    let container = NSPersistentContainer(name: "Model")
+    
+    init() {
+        container.loadPersistentStores { description, error in
+            if let error = error {
+                print("Core Data failed to load: \(error.localizedDescription)")
+            }
+            
+            self.container.viewContext.mergePolicy = NSMergePolicy.mergeByPropertyObjectTrump
+        }
+    }
+}
+
+
+
